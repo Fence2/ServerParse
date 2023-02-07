c9aaaf853e29c2dfffe24da5cf1d888d5c0c3d28
Chistyakov Dmitrii <chist.dmitrii@gmail.com>
2023-02-07 12:25:35 +0300

    Вынес класс Parser, сделал его базовым для всех остальных классов Parser

--- modules/parser/classes.py
+++ modules/parser/classes.py
@@ -1,6 +1,10 @@
 import dataclasses
+import json
+import pickle
 
 from abc import ABC, abstractmethod
+from pathlib import Path
+
 from bs4 import BeautifulSoup
 from dataclasses import dataclass, field
 from .tools import *
@@ -54,10 +58,12 @@ class Server:
     units: str = None
 
     def __repr__(self) -> str:
-        result = f"{self.category}: {self.name} - {str(self.card_price if not self.config_price else self.config_price)}"
+        result = f"{self.category}: {self.name} - " \
+                 f"{str(self.card_price if not self.config_price else self.config_price)}"
 
         if self.no_sale_card_price != 0:
-            result += f" - {str(self.no_sale_card_price if not self.no_sale_config_price else self.no_sale_config_price)}"
+            result += f" - " \
+                      f"{str(self.no_sale_card_price if not self.no_sale_config_price else self.no_sale_config_price)}"
 
         return result
 
@@ -142,3 +148,119 @@ class AbstractConfigurator(SeleniumSupport):
     @abstractmethod
     def get_config_components(servers: list[Server]) -> (list[Server], list[Component]):
         pass
+
+
+class MainParser:
+    def __init__(self, *, module, work_folder, webdriver_path=None, catalog_selenium=False, config_selenium=False):
+        self.module = module
+        self.work_folder = work_folder
+        self.components_file_bin = work_folder / "components.bin"
+        self.components_file_json = work_folder / "components.json"
+        self.servers_list_file_bin = work_folder / "servers_list.bin"
+        self.servers_with_conf_list_bin = work_folder / "servers_confs.bin"
+        self.configs_components_bin = work_folder / "configs_components.bin"
+
+        self.webdriver_path = webdriver_path
+        self.catalog_selenium = catalog_selenium
+        self.config_selenium = config_selenium
+
+    def start(
+            self,
+            get_new_components=True,
+            get_new_servers_list=True,
+            get_new_servers_configs=True,
+
+    ):
+        print(f"\nПарсер {self.module.__name__}\n")
+        components = list()
+        servers = list()
+        servers_with_configs = list()
+        config_components = list()
+        catalog = configurator = None
+
+        if get_new_components:
+            # Комплектующие: Получение
+            print("Комплектующие: Получение")
+
+            catalog = self.module.catalog.Catalog(
+                webdriver_path=self.webdriver_path,
+                launch=self.catalog_selenium
+            )
+
+            components = catalog.get_components()
+
+            # Комплектующие: Сохранение
+            print("Комплектующие: Сохранение..", end="")
+            components_json = [comp.to_dict() for comp in components]
+
+            with open(self.components_file_json, "w", encoding="utf-8") as file:
+                json.dump(components_json, file, ensure_ascii=False)
+
+            with open(self.components_file_bin, "wb") as file:
+                pickle.dump(components, file)
+            print("...Успешное сохранение!\n")
+        else:
+            if Path(self.components_file_bin).is_file():
+                with open(self.components_file_bin, "rb") as file:
+                    components = pickle.load(file)
+
+        if get_new_servers_list or get_new_servers_configs:
+            if get_new_servers_list:
+                # Серверы: Получение
+                print("Серверы: Получение")
+                catalog = self.module.catalog.Catalog(
+                    webdriver_path=self.webdriver_path,
+                    launch=self.catalog_selenium
+                ) if catalog is None else catalog
+
+                servers = catalog.get_servers()
+
+                # Серверы: Сохранение
+                print("Серверы: Сохранение..", end="")
+
+                with open(self.servers_list_file_bin, "wb") as file:
+                    pickle.dump(servers, file)
+                print("...Успешное сохранение!\n")
+            else:
+                if Path(self.servers_list_file_bin).is_file():
+                    with open(self.servers_list_file_bin, "rb") as file:
+                        servers = pickle.load(file)
+
+            if get_new_servers_configs:
+                # Конфигураторы: Получение
+                print("Конфигураторы: Получение")
+                configurator = self.module.configurator.Configurator(
+                    webdriver_path=self.webdriver_path,
+                    launch=self.config_selenium
+                )
+
+                servers_with_configs = configurator.get_config_components(servers)
+                print(f"\nЗагружены данные всех серверов\n")
+
+                # Конфигураторы: Сохранение
+                print("Конфигураторы: Сохранение..", end="")
+
+                with open(self.servers_with_conf_list_bin, "wb") as file:
+                    pickle.dump(servers_with_configs, file)
+
+                with open(self.configs_components_bin, "wb") as file:
+                    pickle.dump(config_components, file)
+                print("...Успешное сохранение!\n")
+
+            else:
+                if Path(self.servers_with_conf_list_bin).is_file():
+                    with open(self.servers_with_conf_list_bin, "rb") as file:
+                        servers_with_configs = pickle.load(file)
+
+                if Path(self.configs_components_bin).is_file():
+                    with open(self.configs_components_bin, "rb") as file:
+                        config_components = pickle.load(file)
+
+        if len(components):
+            components = self.module.data_prettify.prettify_components(components)
+
+        if len(servers_with_configs):
+            servers_with_configs, config_components = self.module.data_prettify.prettify_servers(servers_with_configs)
+            print(f"Уникальных комплектующих из конфигураторов: {len(config_components)}\n")
+
+        return {'components': components, 'servers': servers_with_configs, 'config_components': config_components}
