a23e6a477169c3df8f8a9ada7d505f08c41a6ab1
Chistyakov Dmitrii <chist.dmitrii@gmail.com>
2023-02-08 13:08:56 +0300

    Багфикс servergate

--- servergate/data_prettify.py
+++ servergate/data_prettify.py
@@ -15,9 +15,15 @@ def prettify_components_cfg(components: list[Component], nord_server=False):
 
 def prettify_servers(servers):
     all_components = list()
+    bad_names = [
+        'без дополнительного графического адаптера',
+        'не установлено',
+        'слот для дополнительной сетевой карты'
+    ]
     for server in servers:
         standard_prettify_server(server)
         server.components = prettify_components(server.components)
+        server.components = [comp for comp in server.components if comp.name.lower() not in bad_names]
         all_components += server.components
 
     return servers, prettify_components_cfg(all_components)
